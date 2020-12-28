3bdb104a0182ff6c59e3758da62ae42d139129c7
pilotto8 <66770848+pilotto8@users.noreply.github.com>
2020-12-28 23:19:47 +0100

    createNetwork --> create | tryconnect --> connect

--- serialComunication.ino
+++ serialComunication.ino
@@ -69,68 +69,69 @@ void executeSerialCommands() {
   }
 
   else if (serialString[0].equalsIgnoreCase("wifi")) {
-    if (serialString[1].equalsIgnoreCase("tryconnect")) {
+    if (serialString[1].equalsIgnoreCase("connect")) {
       Serial.print(tryConnect());
 
     }
-    else if (serialString[1].equalsIgnoreCase("createNetwork")) {
+    else if (serialString[1].equalsIgnoreCase("create")) {
       createNetwork();
 
-    }}
+    }
+  }
+  else {
+    Serial.println("Command not recongnised");
+  }
+  inputString = "";
+  stringComplete = false;
+
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
     else {
-      Serial.println("Command not recongnised");
+      inputString += inChar;
     }
-    inputString = "";
-    stringComplete = false;
-  
+    Serial.print(inChar);
   }
+}
 
-  void serialEvent() {
-    while (Serial.available()) {
-      char inChar = (char)Serial.read();
-      if (inChar == '\n') {
-        stringComplete = true;
-      }
-      else {
-        inputString += inChar;
-      }
-      Serial.print(inChar);
+
+void loadSerialCommands(String data) {
+  reportStarting("Loading serial command");
+  String subString;
+  for (c = 0; serialString[c][0] != '\0'; c++) {
+    serialString[c] = "";
+  }
+  for (a = 0, i = 0; data[i] != '\0'; a++, i++) {
+    subString = "";
+    for (; (data[i] != ' ' || a == 2) && data[i] != '\0'; i++) {
+      subString += data[i];
     }
+    serialString[a] = subString;
+    Serial.println(subString);
   }
-
-
-  void loadSerialCommands(String data) {
-    reportStarting("Loading serial command");
-    String subString;
-    for (c = 0; serialString[c][0] != '\0'; c++) {
-      serialString[c] = "";
-    }
-    for (a = 0, i = 0; data[i] != '\0'; a++, i++) {
-      subString = "";
-      for (; (data[i] != ' ' || a == 2) && data[i] != '\0'; i++) {
-        subString += data[i];
-      }
-      serialString[a] = subString;
-      Serial.println(subString);
-    }
-    reportEnding();
-  }
+  reportEnding();
+}
 
 
 
-  void reportStarting(String comment) {
-    Serial.println("--------------------------------");
-    Serial.print(comment + "...\n");
-    execution_time = micros();
-    serial_reporter = 0;
-  }
-  void reportEnding() {
-    Serial.print("\ndone ");
-    Serial.print(micros() - execution_time);
-    Serial.println("us\n");
-  }
-  void reportStep() {
-    Serial.print(serial_reporter);
-    Serial.print(", ");
-    serial_reporter++;
-  }
+void reportStarting(String comment) {
+  Serial.println("--------------------------------");
+  Serial.print(comment + "...\n");
+  execution_time = micros();
+  serial_reporter = 0;
+}
+void reportEnding() {
+  Serial.print("\ndone ");
+  Serial.print(micros() - execution_time);
+  Serial.println("us\n");
+}
+void reportStep() {
+  Serial.print(serial_reporter);
+  Serial.print(", ");
+  serial_reporter++;
+}
