d3f70f5c60e0535865ca263b155f64e44434812c
pilotto8 <66770848+pilotto8@users.noreply.github.com>
2021-01-07 10:16:49 +0100

    prove

--- encryption.ino
+++ encryption.ino
@@ -1,10 +1,9 @@
+using namespace experimental::crypto;
 String encryptString(String data){
-  using namespace experimental::crypto;
   ChaCha20Poly1305::encrypt(data.begin(), data.length(), derivedKey, &encryptionCounter, sizeof encryptionCounter, resultingNonce, resultingTag);
   return data;
 }
 String decryptString(String data){
-  using namespace experimental::crypto;
   bool decrypted = ChaCha20Poly1305::decrypt(data.begin(), data.length(), derivedKey, &encryptionCounter, sizeof encryptionCounter, resultingNonce, resultingTag);
   if (decrypted){
     return data;
